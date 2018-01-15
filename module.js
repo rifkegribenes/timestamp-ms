5232ef234cf0b93351a3b0b572114f4830a2b5b5
Glitch (rifkegribenes-timestamp-ms) <none>
2018-01-15 06:39:48 +0000

    👵🙋 Checkpoint module.js:417995/153

--- module.js
+++ module.js
@@ -10,7 +10,7 @@ const parsetime = (time) => {
   };
 }
 
-http.createServer((req, res) => {
+
   const urlObject = url.parse(req.url, true);
   const pathname = urlObject.pathname;
   const time = new Date(urlObject.query.iso);
@@ -31,23 +31,32 @@ http.createServer((req, res) => {
   }).listen(port);
 
 
-const moment = require('moment');
+// const moment = require('moment');
+const unixTime = (inputDate) => { 
+  const date = new Date(inputDate * 1000);
+	const day = date.getDate();
+		month			=	'0' + (date.getMonth() + 1),
+		year			=	date.getFullYear(),
+		hours			= date.getHours(),
+		minutes		=	'0' + date.getMinutes(),
+		seconds		=	'0' + date.getSeconds();
+
 module.exports = {
-    parse: function (req, res) {
-        const date = req.params.query;
-        let processedDate = {
-            "unix": null,
-            "natural": null
-        }
-        if (+date >= 0) {
-            processedDate.unix = +date;
-            processedDate.natural = moment.unix(+date).format("MMMM D, YYYY")
-        }
-        if (isNaN(+date) && moment(date, "MMMM D, YYYY").isValid()) {
-            processedDate.unix = moment(date, "MMMM D, YYYY").format("X");
-            processedDate.natural = date;
-        }
-        res.send(processedDate);
+  parse: (req, res) => {
+    const date = req.params.query;
+    const dateResult = {
+        "unix": null,
+        "natural": null
     }
+    if (+date >= 0) {
+        dateResult.unix = +date;
+        dateResult.natural = moment.unix(+date).format("MMMM D, YYYY")
+    }
+    if (isNaN(+date) && moment(date, "MMMM D, YYYY").isValid()) {
+        dateResult.unix = moment(date, "MMMM D, YYYY").format("X");
+        dateResult.natural = date;
+    }
+    res.send(dateResult);
+  }
 }
 
