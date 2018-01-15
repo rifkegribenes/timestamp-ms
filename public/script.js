e6495cd887f4a21da6a4a6f112e4b7a251ccce9a
Glitch (rifkegribenes-timestamp-ms) <none>
2018-01-15 22:40:28 +0000

    🕶💍 Checkpoint public/script.js:417995/143 public/style.css:417995/59 views/index.pug:417995/107

--- public/script.js
+++ public/script.js
@@ -4,18 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const Root_Url = window.location.href;
 	const naturalLink = `${Root_Url}December 15, 2015<br><a target="_blank" href="${Root_Url}December 15, 2015" class="card__action">Try It</a>`;
 	const unixLink = `${Root_Url}1450137600<br><a target="_blank" href="${Root_Url}1450137600" class="card__action">Try It</a>`;
-  const months = ["January
-  const today = new Date();
-  const dd = today.getDate();
-  const mm = today.getMonth()+1;
-  const yyyy = today.getFullYear();
-
-if(mm<10){
-    mm='0'+mm;
-} 
-var today = dd+'/'+mm+'/'+yyyy;
-document.getElementById("DATE").value = today;
-  const today = `${Root_Url}${new Date()};<br><a target="_blank" href="${Root_Url}${Root_Url}${new Date()}" class="card__action">Try Today's Date</a>`;
+  const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  const now = new Date();
+  const dd = now.getDate();
+  const month = months[now.getMonth()];
+  const yyyy = now.getFullYear();
+  const formattedToday = `${month} ${dd}, ${yyyy}`;
+  const today = `${Root_Url}${formattedToday};<br><a target="_blank" href="${Root_Url}${formattedToday}}" class="card__action">Try Today's Date</a>`;
 	document.getElementById("natural-link").innerHTML = naturalLink;
 	document.getElementById("unix-link").innerHTML = unixLink;
   document.getElementById("today").innerHTML = today;
