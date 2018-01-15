585db366db5fb408368db7a770c6e1dba2bb9d5b
Glitch (rifkegribenes-timestamp-ms) <none>
2018-01-15 21:35:26 +0000

    🚆🚇 Checkpoint public/script.js:417995/114 views/index.pug:417995/2

--- public/script.js
+++ public/script.js
@@ -2,11 +2,9 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const Root_Url = window.location.href;
-	const naturalLink = `${Root_Url}December 15, 2015`;
-	const unixLink = `${Root_Url}1450137600`;
-	document.getElementById("natural-link").text(naturalLink);
-	document.getElementById("unix-link").text(unixLink);
-	const links = `<a target="_blank" href="${naturalLink}">Try First link</a><a target="_blank" href="${unixLink}">Try Second link</a>`
-	document.getElementById("try").append(links);
+	const naturalLink = `${Root_Url}December 15, 2015<br><a target="_blank" href="${Root_Url}December 15, 2015" class="card__action">Try It</a>`;
+	const unixLink = `${Root_Url}1450137600<br><a target="_blank" href="${Root_Url}1450137600" class="card__action">Try It</a>`;
+	document.getElementById("natural-link").innerHTML = naturalLink;
+	document.getElementById("unix-link").innerHTML = unixLink;
 });
 
