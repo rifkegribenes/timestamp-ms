2c5a54f7d6f8980fb769f5f8efd6fafbcf285266
Glitch (rifkegribenes-timestamp-ms) <none>
2018-01-15 06:23:04 +0000

    server.js

--- server.js
+++ server.js
@@ -18,37 +18,3 @@ const server = http.createServer(app);
 const port = process.env.PORT || '3000';
 app.set('port', port);
 server.listen(port, () => console.log(`API running on localhost:${port}`));
-
-
-const unixtime = (time) => {
- return { unixtime: time.getTime() };
-}
-
-const parsetime = (time) => {
-  return {
-    hour: time.getHours(),
-    minute: time.getMinutes(),
-    second: time.getSeconds()
-  };
-}
-
-http.createServer((req, res) => {
-  const urlObject = url.parse(req.url, true);
-  const pathname = urlObject.pathname;
-  const time = new Date(urlObject.query.iso);
-  let result;
-
-    if (pathname === '/api/unixtime') {
-      result = unixtime(time);
-    } else if (pathname === '/api/parsetime') {
-      result = parsetime(time);
-    }
-    if (result) {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(result));
-    } else {
-      res.writeHead(404);
-      res.end();
-    }
-  }).listen(port);
-
