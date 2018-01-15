72143fb13cb1ae7ed3dad0c3f3a71dc36a771832
Glitch (rifkegribenes-timestamp-ms) <none>
2018-01-15 19:14:55 +0000

    🍓💸 Checkpoint package.json:417995/4 app/routes/index.js:417995/25

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,50 +1,11 @@
-module.exports = (app) => {
-
-	app.route('/')
-		.get((req, res) => {
-			res.sendFile(process.cwd() + '/public/index.html');
-		});
-
-	app.route('/:time')
-		.get((req, res) => {
-			function isValidDate(date) {
-				var d;
-				var result;
-
-				d = !isNaN(+date) ? new Date(+date) : new Date(date);
-
-				if (Object.prototype.toString.call(d) === '[object Date]') {
-
-					if (isNaN(d.getTime())) {
-
-						result = 'Invalid date.';
-
-					} else {
-
-						var options = {year: 'numeric', month: 'long', day: 'numeric'};
-
-						result = {
-							'unix': d.getTime(),
-							'natural': d.toLocaleDateString('en-US', options)
-						};
-					}
-					return result;
-				}
-				return 'Invalid date.';
-			}
-			res.send(isValidDate(req.params.time));
-		});
-};
-
 const express = require('express');
 const router = express.Router();
 
 // modules this api provides routing for
-const timestamp = require('../timestamp/timestamp');
+const module = require('../module');
 
 // TimeStamp Microservice Routes
 // (note, catching a variety of urls and routing them to the same place)
-router.route('/timestamp/api/v1/:query').get(timestamp.parse);
 router.route('/timestamp/api/:query').get(timestamp.parse);
 router.route('/timestamp/:query').get(timestamp.parse);
 
@@ -56,4 +17,5 @@ router.get('*', (req, res) => {
     title: 'Timestamp Microservice'
   });
 });
+
 module.exports = router;
