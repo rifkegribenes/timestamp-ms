4a11bdbbc3450f8b7b9b992aaac713da0c7c083e
Glitch (rifkegribenes-timestamp-ms) <none>
2018-01-15 22:29:27 +0000

    🌮👊 Checkpoint public/script.js:417995/92

--- public/script.js
+++ public/script.js
@@ -4,6 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const Root_Url = window.location.href;
 	const naturalLink = `${Root_Url}December 15, 2015<br><a target="_blank" href="${Root_Url}December 15, 2015" class="card__action">Try It</a>`;
 	const unixLink = `${Root_Url}1450137600<br><a target="_blank" href="${Root_Url}1450137600" class="card__action">Try It</a>`;
+  const months = ["January
+  const today = new Date();
+  const dd = today.getDate();
+  const mm = today.getMonth()+1;
+  const yyyy = today.getFullYear();
+
+if(mm<10){
+    mm='0'+mm;
+} 
+var today = dd+'/'+mm+'/'+yyyy;
+document.getElementById("DATE").value = today;
   const today = `${Root_Url}${new Date()};<br><a target="_blank" href="${Root_Url}${Root_Url}${new Date()}" class="card__action">Try Today's Date</a>`;
 	document.getElementById("natural-link").innerHTML = naturalLink;
 	document.getElementById("unix-link").innerHTML = unixLink;
